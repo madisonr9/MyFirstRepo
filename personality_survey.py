279aefa95fc51f4e68d6f8f3f7a269641d4a8c0a
madisonr9 <32672766+madisonr9@users.noreply.github.com>
2017-11-16 09:40:23 -0500

    Update personality_survey.py

--- personality_survey.py
+++ personality_survey.py
@@ -7,3 +7,10 @@ elif animal == "dog":
     print("That's okay I still like you")
 else:
     print(animal + "? That's Okay.")
+
+print("How old are you?")
+age = input()
+
+if age == "13":
+    print(Yay! we are the same age! When's your birthday? ")
+    print("When's your birthday?")
